060d366d2c17bcf3f0fa3099ee7463087ed16a07
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2021-11-21 21:48:00 +0530

    end

--- toaster/src/App.js
+++ toaster/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import React from "react";
+import classNames from "classnames";
+import toast, { Toaster } from "react-hot-toast";
+import { MdOutlineClose } from "react-icons/md";
+import { HiLightningBolt } from "react-icons/hi";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
+
+const notify = () =>
+  toast.custom(
+    (t) => (
+      <div
+        className={classNames([
+          styles.notificationWrapper,
+          t.visible ? "top-0" : "-top-96",
+        ])}
+      >
+        <div className={styles.iconWrapper}>
+          <HiLightningBolt />
+        </div>
+        <div className={styles.contentWrapper}>
+          <h1>New version available</h1>
+          <p>
+            An improved version of VESSEL is now available, refresh to update.
+          </p>
+        </div>
+        <div className={styles.closeIcon} onClick={() => toast.dismiss(t.id)}>
+          <MdOutlineClose />
+        </div>
+      </div>
+    ),
+    { id: "unique-notification", position: "top-center" }
   );
-}
+
+
+  const App = () => {
+    return(
+      <div>
+        <button onClick={notify}>Notify</button>
+        <Toaster/>
+      </div>
+
+    );
+
+  };
+
 
 export default App;
